f187d05dd41f9272b7189ad710ddaca852cc23e0
Minerva Quintero <minerva.quintero@bitingbit.com>
2023-11-06 19:40:32 +0000

    Initial commit

---
+++ app/src/main/java/com/example/libraryapp/model/Book.kt
@@ -0,0 +1,11 @@
+package com.example.libraryapp.model
+
+class Book (
+    val ISBN: Number,
+    val author_id: Number,
+    val title: String,
+    val sinopsis: String,
+    val score: Number
+    )       {
+
+}
