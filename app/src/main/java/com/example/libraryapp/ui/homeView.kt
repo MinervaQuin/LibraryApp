350f7c0a8261eda51eda0c59e273e23222266641
JoseManuel117 <jmksd117@gmail.com>
2023-11-27 21:44:07 +0000

    minor changes

--- app/src/main/java/com/example/libraryapp/ui/homeView.kt
+++ app/src/main/java/com/example/libraryapp/ui/homeView.kt
@@ -1,25 +1,41 @@
 package com.example.libraryapp.ui
 
+import CartViewModel
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
 import com.example.libraryapp.model.firebaseAuth.UserData
 import com.example.libraryapp.viewModel.homeViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -31,42 +47,14 @@ fun HomeView(
     onSignOut: () -> Unit,
     viewModel: homeViewModel
     ) {
+    val colecionPrueba = Collection("Prueba", "Cositas bro", "https://m.media-amazon.com/images/I/6135vNR5sCL._AC_UF1000,1000_QL80_.jpg")
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
 
-        println(userData)
-        if(userData?.profilePictureUrl != null){
-            AsyncImage(
-                model = userData.profilePictureUrl,
-                contentDescription = "Imagen de Perfil",
-                modifier = Modifier
-                    .size(150.dp)
-                    .clip(CircleShape),
-                contentScale = ContentScale.Crop
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-        }
-        if(userData?.userName != null) {
-            Text(
-               text = userData.userName,
-                textAlign = TextAlign.Center,
-                fontSize = 36.sp,
-                fontWeight = FontWeight.SemiBold
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-        }
-        if(userData?.userId != null){
-            Text(
-                text = userData.userId,
-                textAlign = TextAlign.Center,
-                fontSize = 36.sp,
-                fontWeight = FontWeight.SemiBold
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-        }
+        CollectionBox(collection = colecionPrueba)
         Button(onClick = onSignOut){
             Text(text = "Cerrar Sesión")
         }
@@ -75,9 +63,71 @@ fun HomeView(
             //viewModel.getAuthorAndLog("Rkwq8a3v54TV6FSGw2n9")
             //viewModel.getCollectionAndLog("oBMLVCnbNsPQJiPexKL7")
             //viewModel.getReviewsAndLog("B9svfDJglRgEPyN6wSAh")
-            viewModel.uploadReviewTest()
+            //viewModel.uploadReviewTest()
+
         }) {
             Text(text = "Probar el Coso")
         }
+
+
     }
+
+}
+data class Collection(
+    val title: String,
+    val subtitle: String,
+    val imageUrl: String
+)
+@Composable
+fun CollectionBox(collection: Collection) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .height(200.dp)
+            .width(120.dp),
+        elevation = CardDefaults.elevatedCardElevation(8.dp),
+        shape = RectangleShape // Bordes rectos
+    ) {
+        Column {
+            Box(
+                modifier = Modifier
+                    .weight(1f) // Toma la mayor parte del espacio disponible
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = rememberAsyncImagePainter(model = collection.imageUrl),
+                    contentDescription = "Book cover",
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentScale = ContentScale.Crop // Ajusta la imagen para llenar el espacio asignado
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .weight(0.2f) // Toma una fracción más pequeña del espacio
+                    .fillMaxWidth()
+                    .background(Color.Black) // Fondo negro para el texto
+            ) {
+                Text(
+                    text = collection.title.toUpperCase(),
+                    color = Color.White,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .align(Alignment.Center) // Centra el texto en la caja más pequeña
+                        .padding(8.dp)
+                )
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewhomeView() {
+    val colecionPrueba = Collection("Prueba", "Cositas bro", "https://m.media-amazon.com/images/I/6135vNR5sCL._AC_UF1000,1000_QL80_.jpg")
+    CollectionBox(colecionPrueba)
+
 }
