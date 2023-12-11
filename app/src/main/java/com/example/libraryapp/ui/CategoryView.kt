ee72b820006f8a7340ce5dea256f71b38461df37
Andrew-022 <145354381+Andrew-022@users.noreply.github.com>
2023-12-11 12:25:11 +0100

    Pantalla carga categorias

--- app/src/main/java/com/example/libraryapp/ui/CategoryView.kt
+++ app/src/main/java/com/example/libraryapp/ui/CategoryView.kt
@@ -1,5 +1,8 @@
 package com.example.libraryapp.ui
 
+import android.app.ProgressDialog
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -23,6 +26,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -30,23 +36,44 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import com.example.libraryapp.ui.theme.GreenApp
 import com.example.libraryapp.viewModel.CategoryViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun CategoryView (navController: NavController, ViewModel: CategoryViewModel,categoria: String ){
     //val selectedCategory by rememberUpdatedState(newValue = ViewModel.selectedCategory)
     //ViewModel.updateSelectedCategory(categoria)
     val executedOnce = remember { mutableSetOf<Boolean>() }
+    val context = LocalContext.current
+    val loading by ViewModel.loading.collectAsState()
 
     if (executedOnce.add(true)) {
         ViewModel.updateSelectedCategory(categoria)
+    }
+
+    if (loading) {
+        DisposableEffect(Unit) {
+            val progressDialog = ProgressDialog(context)
+            progressDialog.setTitle("Cargando...")
+            progressDialog.setMessage("Por favor, espere")
+            progressDialog.setCancelable(true)
+            progressDialog.show()
+
+            onDispose {
+                progressDialog.dismiss()
+            }
+        }
     }
 
     Column (
